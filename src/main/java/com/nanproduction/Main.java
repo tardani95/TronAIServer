b4d108a33e0be6f11d9772f23b50f620ea96c39d
Daniel Tar <tardani95@gmail.com>
2017-11-27 22:08:18 +0100

    TronAIServer 0.9 - RESTARTFUN, KILLAPPLICATION

--- src/main/java/com/nanproduction/Main.java
+++ src/main/java/com/nanproduction/Main.java
@@ -1,7 +1,6 @@
 package com.nanproduction;
 
 import com.nanproduction.GameElements.Game;
-import com.nanproduction.GameElements.GameStateEnum;
 import com.nanproduction.Server.WebSocketHandler;
 import org.webbitserver.WebServer;
 import org.webbitserver.WebServers;
@@ -9,25 +8,21 @@ import org.webbitserver.handler.StaticFileHandler;
 
 public class Main{
 
+    public static final int PORT = 8090;
     public static final int CELL_SIZE = 20;
     public static final double BODY_SCALE = 0.5;
 
 
     private static Game game;
+    private static WebServer webServer;
+    private static boolean STOP = false;
 
     public static void main(String[] args) {
         System.out.println("main()");
 
-        WebServer webServer = WebServers.createWebServer(8090);
-        webServer.add(new StaticFileHandler("src/main/resources/static"));
-        webServer.add("/websocket", new WebSocketHandler());
-        webServer.start();
+        startServer();
 
-
-        game=Game.getInstance();
-        game.init();
-
-        while (game.getGameState()!= GameStateEnum.ENDING){
+        while (!STOP){
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -35,13 +30,42 @@ public class Main{
             }
         }
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        webServer.stop();
+        stopServer();
+        System.out.println("Application stopped.");
     }
+
+    public static void startServer(){
+        webServer = WebServers.createWebServer(PORT);
+        webServer.add(new StaticFileHandler("src/main/resources/static"));
+        webServer.add("/websocket", new WebSocketHandler());
+        webServer.start();
+
+        game=Game.getInstance();
+        game.init();
+    }
+
+    public static void restartServer(){
+        stopServer();
+        startServer();
+    }
+
+    public static void stopServer(){
+        System.err.println("Stopping webSocketServer...");
+        if(webServer != null){
+            game.stop();
+            webServer.stop();
+            System.out.println("WebSocketServer stopped.");
+            return;
+        }
+        System.err.println("Failed to stop server.");
+    }
+
+    public static void stopServerAndExit(){
+        STOP = true;
+        System.out.println("Stopping application...");
+    }
+
+
 
 
 
--- src/main/java/com/nanproduction/Server/WebSocketHandler.java
+++ src/main/java/com/nanproduction/Server/WebSocketHandler.java
@@ -6,10 +6,12 @@ import com.nanproduction.GameElements.Player;
 import org.webbitserver.BaseWebSocketHandler;
 import org.webbitserver.WebSocketConnection;
 
+import static com.nanproduction.Main.restartServer;
+import static com.nanproduction.Main.stopServerAndExit;
+
 public class WebSocketHandler extends BaseWebSocketHandler {
 
-    private Game game= Game.getInstance();
-
+    private Game game = Game.getInstance();
 
 
     @Override
@@ -18,11 +20,10 @@ public class WebSocketHandler extends BaseWebSocketHandler {
 
     @Override
     public void onClose(WebSocketConnection connection) {
-        Player player=game.getPlayer(connection);
-        if(player==null){
+        Player player = game.getPlayer(connection);
+        if (player == null) {
             game.removeWatcher(connection);
-        }
-        else {
+        } else {
             game.removePlayer(connection);
         }
 
@@ -34,32 +35,40 @@ public class WebSocketHandler extends BaseWebSocketHandler {
     public void onMessage(WebSocketConnection connection, String message) {
         //if the value of the players map is null, then adds the message, otherwise do nothing - first message must be the player name
         //players.putIfAbsent(connection, message);
-        if(message.equals("GUEST")){
-            game.addWatcher(connection);
-            System.out.println("New Watcher");
-            return;
+        switch (message) {
+            case "GUEST":
+                game.addWatcher(connection);
+                System.out.println("New Watcher");
+                return;
+            case "KILLAPPLICATION":
+                stopServerAndExit();
+                return;
+            case "RESTARTFUN":
+                restartServer();
+                return;
+            default:break;
         }
 
-        Player player  = game.getPlayer(connection);
 
-        if(player == null){
-            if(game.getGameState()== GameStateEnum.WAITING_FOR_PLAYERS) {
+        Player player = game.getPlayer(connection);
+
+        if (player == null) {
+            if (game.getGameState() == GameStateEnum.WAITING_FOR_PLAYERS) {
                 game.addNewPlayer(connection);
                 connection.send("Welcome to the Arena!\n"
                         + "Total No. of racers: " + game.getNumOfPlayers() + ".");
                 System.out.println("Someone connected. Connections: "
                         + game.getNumOfPlayers());
-            }
-            else{
+            } else {
                 connection.send("Your game is ended!");
             }
 
-        }else {
+        } else {
             if (game.getGameState() == GameStateEnum.WAITING_FOR_PLAYERS) {
                 if (message.equals("READY")) {
                     player.setReady(true);
                 }
-                System.out.println("Player " + player.getId()+" is ready!");
+                System.out.println("Player " + player.getId() + " is ready!");
             } else {
                 System.out.println(player.getId() + ": " + message/*+" time:"+ System.currentTimeMillis()%10000*/);
                 //Game.getInstance().addKeyCode(message);
